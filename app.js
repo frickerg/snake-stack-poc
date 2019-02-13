1f050a5ae54d407aee7ee28e841db0bc555d7c81
Guillaume Fricker <fricker.g@zoho.com>
2019-02-13 22:53:54 +0100

    created sqlite schema

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const sqlite3 = require('sqlite3').verbose();
+const table_names = ['TBL_Bookshelf', 'TBL_Inventory'];
 
-// open database in memory
-let db = new sqlite3.Database(':memory:', (err) => {
+// open snakedb
+let db = new sqlite3.Database('./resources/snakedb.sqlite3', sqlite3.OPEN_READONLY, (err) => {
 	if (err) {
-		return console.error(err.message);
+		console.error(err.message);
 	}
-	console.log('Connected to the in-memory SQlite database.');
+	console.log('connection to snakedb successful');
 });
 
-// close the database connection
+// serialize snakedb tables
+db.serialize(() => {
+	table_names.forEach(tbl => {
+		console.log(tbl);
+		db.each('PRAGMA table_info(' + tbl + ')', (err, col) => {
+			if (err) {
+				console.error(err.message);
+			}
+			console.log(col);
+		});
+	})
+});
+
+// close the connection
 db.close((err) => {
 	if (err) {
-		return console.error(err.message);
+		console.error(err.message);
 	}
-	console.log('Close the database connection.');
+	console.log('Connection closed');
 });
