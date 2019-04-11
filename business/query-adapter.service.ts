0d1900b1483c73736376a912f65d210b3cdb25f0
Guillaume Fricker <fricker.g@zoho.com>
2019-04-11 03:10:44 +0200

    Preparing query adapter for further operations within the architecture

--- business/query-adapter.service.ts
+++ business/query-adapter.service.ts
@@ -1,5 +1,15 @@
 import QueryBuilderService from '../persistence/query-builder.service';
 
+interface Bookshelf {
+	"CatType": number;
+	"CatID": number;
+	"ItemID": string;
+	"Description": string;
+	"Balance": number | null;
+	"Position": number | null;
+	"PositionTotal": number | null;
+}
+
 export default class QueryAdapterService {
 	private queryBuilder: QueryBuilderService;
 
@@ -8,7 +18,23 @@ export default class QueryAdapterService {
 	}
 
 	public async triggerTestQuery() {
-		return this.queryBuilder.execTestQuery('TBL_Bookshelf', 3);
+		let promise = new Promise<Object[]>((resolve, reject) => {
+			return resolve(this.queryBuilder.execTestQuery('TBL_Bookshelf', 20))
+		});
+
+		let result = await promise;
+
+		//TODO: following code  should be performed in its own mapper class!
+		let mappedData: Bookshelf[] = [];
+
+		result.forEach(element => {
+			mappedData.push(element as Bookshelf);
+		});
+
+		//XXX: gets logged twice somehow, also happens before calling the endpoint! (?)
+		// Needs less calls since everything is read at the beginning, but is that optimal at runtime?
+		console.log('first descr : ', mappedData[0].Description);
+		return mappedData;
 	}
 
 }
